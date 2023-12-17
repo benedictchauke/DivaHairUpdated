1e21123b527254e23e49cc16b030dd2e874f8352
Benedict Isaac Chauke <benedictchauke@hotmail.com>
2023-12-17 14:55:17 +0200

    adding javascript files and installing jquery

---
+++ DivaHair/DivaHair/wwwroot/index.js
@@ -0,0 +1,21 @@
+﻿$(function () {
+    var theForm = $("#theForm");
+    theForm.hide();
+
+    var button = $("#Buy");
+    button.on("click", function() {
+        console.log("Buying Item");
+    });
+
+    var info = $(".prop li");
+    info.on("click", function() {
+        console.log("you clicked on" + $(this).text());
+    });
+
+    var $loggin = $("#log-in");
+    var $logForm = $(".logForm");
+
+    $log-in.on("click", function () {
+        $logForm.toggle(1000);
+    });
+});
