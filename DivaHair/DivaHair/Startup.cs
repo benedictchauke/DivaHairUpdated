bc2921019eac8dadea16f3cc310580dab7d02f88
Benedict Isaac Chauke <benedictchauke@hotmail.com>
2024-05-08 17:46:58 +0200

    modifying the views for the appcontroller

--- DivaHair/DivaHair/Startup.cs
+++ DivaHair/DivaHair/Startup.cs
@@ -15,12 +15,20 @@ namespace DivaHair
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            app.UseDefaultFiles();
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/error");
+            }
             app.UseStaticFiles();
             app.UseMvc(cfg => {
                 cfg.MapRoute("Default",
