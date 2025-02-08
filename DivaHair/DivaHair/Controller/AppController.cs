a5d8b648c2c0d29f741c88e1d15854a47c1997da
Benedict Isaac Chauke <benedictchauke@hotmail.com>
2025-02-08 22:40:10 +0200

    fixing errors

--- DivaHair/DivaHair/Controller/AppController.cs
+++ DivaHair/DivaHair/Controller/AppController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace DivaHair.Controller
+namespace DivaHair.Controllers
 {
     public class AppController : Controller //controller is descriptive view looks for app
     {
